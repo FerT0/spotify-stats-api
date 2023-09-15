27930249750d04280f5e1f3e9fc1e6a2c76c34c1
FerT0 <jtasconacevedo@gmail.com>
2023-09-15 17:43:35 -0500

    added more user authorization scopes

--- index.js
+++ index.js
@@ -24,7 +24,8 @@ app.get("/login", (req, res) => {
   const state = generateRandomString(16);
   res.cookie(stateKey, state);
 
-  const scope = "user-read-private user-read-email";
+  const scope =
+    "user-read-private user-read-email playlist-read-private user-top-read";
 
   const queryParams = querystring.stringify({
     client_id: CLIENT_ID,
